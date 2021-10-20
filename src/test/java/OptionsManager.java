1c01fd336de6e10d87d0c9be25a6ca4cb745d1e9
TheDhamo <dhamptharan.dhamo@maersk.com>
2021-10-20 16:15:56 +0200

    initial commit

---
+++ src/test/java/OptionsManager.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+public class OptionsManager {
+    //Get Chrome Options
+    public static ChromeOptions getChromeOptions() {
+        System.out.println("CHORME BROWSER RUNNNing");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        return options;
+    }
+    //Get Firefox Options
+    public static FirefoxOptions getFirefoxOptions () {
+        FirefoxOptions options = new FirefoxOptions();
+        FirefoxProfile profile = new FirefoxProfile();
+        //Accept Untrusted Certificates
+        profile.setAcceptUntrustedCertificates(true);
+        profile.setAssumeUntrustedCertificateIssuer(false);
+        //Use No Proxy Settings
+        profile.setPreference("network.proxy.type", 0);
+        //Set Firefox profile to capabilities
+        options.setCapability(FirefoxDriver.PROFILE, profile);
+        return options;
+    }
+}
